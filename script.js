e4109993d94263767742614b4807f922fa26d4ab
Sador <Zerezghi@live.co.uk>
2024-02-09 03:44:42 +0000

    Daily challenge completed ✅

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*
+/* Kata 1
 8kyu 
 Write a function which takes a number and returns the corresponding ASCII char for that value.
 Example:
@@ -7,6 +7,24 @@ Example:
 48 --> '0
 */
 
-function getChar(c){
-    
+const getChar = (c) => {
+
+}
+
+
+
+/* Kata 2
+8kyu
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. 
+If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't
+*/
+
+const lovefunc = (flower1, flower2) => {
+  if (flower1 % 2 == !(flower2 % 2)) {
+    return true;
+  } else {
+    return false;
   }
+};
