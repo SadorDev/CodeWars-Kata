497533c97e7a635de7ad4fd91c4bd22e03e68eb4
Sador <Zerezghi@live.co.uk>
2024-02-09 03:26:14 +0000

    Daily challenge

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const someAVariable = ' speech'
+/*
+8kyu 
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+Example:
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+*/
+
+function getChar(c){
+    
+  }
