a0430308d7b6360ae701d1d5aef7132e5baa4ad3
Sador <Zerezghi@live.co.uk>
2024-02-11 15:11:03 +0000

    ✅

--- script.js
+++ script.js
@@ -74,3 +74,8 @@ isPalindrome("A man, a plan, a canal, orange");
 8kyu
 Code as fast as you can! You need to double the integer and return it.
 */
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i *2;
+}
